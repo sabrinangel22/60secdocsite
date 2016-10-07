8366e16d48f153c0c610e56788a7ed2ed2d63fb8
Yano <brandon.yano@lanmlw-nmxkj1d5.na.corp.ipgnetwork.com>
2016-10-07 12:53:20 -0700

    Increased FB video limit

--- scripts/fb.js
+++ scripts/fb.js
@@ -4,9 +4,9 @@ window.fbAsyncInit = function() {
     xfbml      : true,
     version    : 'v2.7'
   });
-  
+
   FB.api(
-    "/1536789006633925/videos?access_token=1781519975439910|50ca548c3eb6af45e1812ea315bd658b",
+    "/1536789006633925/videos?limit=100&access_token=1781519975439910|50ca548c3eb6af45e1812ea315bd658b",
     function (response) {
       $(".fb-loading").hide();
       $("a.load-more").css("display", "inline-block");
@@ -16,7 +16,7 @@ window.fbAsyncInit = function() {
             $("#facebook-videos-container").append('<div class="fb-video"><a href="#fb-video-popup" class="fb-popup" data-fb-id="'+value.id+'"><img src="https://graph.facebook.com/'+value.id+'/picture?width=500&height=500&access_token=1781519975439910|50ca548c3eb6af45e1812ea315bd658b" /></a></div>');
           }
         });
-        
+
         $('a.fb-popup').click( function(e) {
           e.preventDefault();
           $('<div class="modal"><iframe src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2F60SecDocs%2Fvideos%2F'+$(this).data('fb-id')+'%2F&show_text=0&width=560" width="560" height="315" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allowFullScreen="true"></iframe></div>').appendTo('body').modal();
@@ -28,15 +28,15 @@ window.fbAsyncInit = function() {
       }
     }
   );
-  
+
   $(document).ready( function() {
     $("a.load-more").click( function(event) {
       event.preventDefault();
-      
+
       $("#facebook-videos-container").css("max-height", "none");
       $(this).hide();
     });
-    
+
     $(".links a, .press a").click( function(e) {
       console.log("CLICK");
       if (document.cookie.replace(/(?:(?:^|.*;\s*)sixtysecdocs\s*\=\s*([^;]*).*$)|^.*$/, "$1") !== "true") {
